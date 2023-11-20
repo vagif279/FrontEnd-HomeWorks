e9b45734b87454152b423ebf273d38fa2b798c74
vagif279 <vagif.gambarov2015@gmail.com>
2023-11-20 16:49:21 +0400

    Прототип кода
    Моё представление как будет работать конечный код. Пока что без самого кода

--- JS_MyArray/index.js
+++ JS_MyArray/index.js
@@ -1,3 +1,24 @@
 /* В функцию конструктор MyArray добавить методы shift и unshift */
 
 console.clear();
+
+function MyArray() {
+    
+}
+
+MyArray.prototype.shift = function() {
+    
+}
+
+MyArray.prototype.unshift = function() {
+
+}
+
+
+const arr = new MyArray(1, 2, 3);
+
+arr.shift();
+console.log(arr); // { 3, 1, 2 }
+
+arr.unshift();
+console.log(arr); // { 1, 2, 3 }
