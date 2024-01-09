5d09abfb7648eaf0c360a2700e6c85bbb0785c86
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-09 13:21:21 +0400

    Функция случайного выбора цвета

--- JS_events/ex1/index.js
+++ JS_events/ex1/index.js
@@ -1,5 +1,15 @@
 const links = document.querySelectorAll('ul li a');
 
 links.forEach(link => {
-    //
+    
 });
+
+//Функция случайного выбора цвета ссылки
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
