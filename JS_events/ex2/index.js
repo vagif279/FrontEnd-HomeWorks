90802ec708e4aea155890e52c25afb5c85d1034c
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-09 17:45:40 +0400

    Полный код ко второму заданию

--- JS_events/ex2/index.js
+++ JS_events/ex2/index.js
@@ -1,9 +1,33 @@
 const button = document.getElementById('showMessagesBtn');
 const listenersCount = 10;
 
-// Добавляем 10 слушателей
+//Добавляем 10 слушателей
 for (let i = 1; i <= listenersCount; i++) {
     button.addEventListener('click', function handler() {
         console.log(i);
     });
 }
+
+//Функция для ввода числа от 1 до 10 от пользователя
+function getUserNumber() {
+    const userNumber = parseInt(prompt('Введите число от 1 до 10:'), 10);
+
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 10) {
+        alert('Некорректный ввод. Пожалуйста, введите число от 1 до 10.');
+        return getUserNumber(); // Вызов заново если некорректный ввод
+    }
+    
+    return userNumber;
+}
+
+//Функция для отображения сообщений
+function showMessages(count) {
+    for (let i = 1; i <= count; i++) {
+        console.log(i);
+    }
+}
+
+button.addEventListener('click', function () {
+    const userNumber = getUserNumber();
+    showMessages(userNumber);
+});
