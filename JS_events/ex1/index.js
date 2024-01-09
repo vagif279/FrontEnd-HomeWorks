cc3c75d4f656eb61d82b3a17b9b5152e97108fb7
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-09 13:22:07 +0400

    Добавлен эффект случайного цвета
    И убран переход по ссылке

--- JS_events/ex1/index.js
+++ JS_events/ex1/index.js
@@ -1,7 +1,20 @@
 const links = document.querySelectorAll('ul li a');
 
 links.forEach(link => {
-    
+    //Отвечает за то покраску ссылки
+    link.addEventListener('mouseenter', () => {
+        link.style.color = getRandomColor();
+    });
+
+    //Отвечает за очищение цвета ссылки после того как мышку убрали
+    link.addEventListener('mouseleave', () => {
+        link.style.color = '';
+    });
+
+    //Убирает дефолтное поведение ссылки (переход по ней)
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+    });
 });
 
 //Функция случайного выбора цвета ссылки
