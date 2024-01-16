e18483cf0b3410296698859cd5c7c1e9d9dd6c06
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-16 13:37:01 +0400

    Первая версия HTML и CSS

--- Интерактивный список задач/index.js
+++ Интерактивный список задач/index.js
@@ -1,11 +1,11 @@
 // Класс для представления задачи
 class Task {
     constructor(id, title, description, createdAt, isCompleted) {
-      this.id = id;
-      this.title = title;
-      this.description = description;
-      this.createdAt = createdAt;
-      this.isCompleted = isCompleted;
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.isCompleted = isCompleted;
     }
 }
   
