3a4190cc38543ef704ef5b7469a5eb141365554c
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-16 04:26:38 +0400

    Перенос условия +первый класс
    Перенёс файл с условием задачи себе и также создал первый класс Таск и конструктор к нему

---
+++ Интерактивный список задач/index.js
@@ -0,0 +1,10 @@
+// Класс для представления задачи
+class Task {
+    constructor(id, title, description, createdAt, isCompleted) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.createdAt = createdAt;
+      this.isCompleted = isCompleted;
+    }
+}
