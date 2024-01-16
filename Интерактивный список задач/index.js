2b6f43ff8cb457407ef84944c2071eea0811d2ff
vagif279 <vagif.gambarov2015@gmail.com>
2024-01-16 10:56:27 +0400

    Добавил остальные методы в TaskList

--- Интерактивный список задач/index.js
+++ Интерактивный список задач/index.js
@@ -17,12 +17,39 @@ class TaskList {
 
     // Добавление новой задачи
     addTask(task) {
-        this.tasks.push(task);
+        this.tasks.push(task); //добавляет в конец
     }
 
     // Удаление задачи по ID
     removeTaskById(id) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        this.tasks = this.tasks.filter((task) => task.id !== id); // Если task.id не равен id то вернёт true (оставить в списке tasks), если false - уберёт
     }
 
+    // Получение задачи по ID
+    getTaskById(id) {
+        return this.tasks.find((task) => task.id === id);
+    }
+
+    // Обновление задачи
+    updateTask(updatedTask) {
+        const index = this.tasks.findIndex((task) => task.id === updatedTask.id);
+        if (index !== -1) {
+            this.tasks[index] = updatedTask;
+        }
+    }
+
+    // Фильтрация задач по статусу выполненности
+    filterTasksByStatus(isCompleted) {
+        return this.tasks.filter((task) => task.isCompleted === isCompleted);
+    }
+
+    // Сортировка задач по названию
+    sortTasksByName() {
+        this.tasks.sort((a, b) => a.title.localeCompare(b.title)); // -1 если a.title<b.title, 0 если равны и 1 если >
+    }
+
+    // Сортировка задач по дате создания
+    sortTasksByDate() {
+        this.tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    }
 }
